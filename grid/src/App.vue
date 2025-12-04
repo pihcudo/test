9ac2c96e11a7b06a7c9054b44d62b4bfc0ab28d3
pih <pih1@cudo.co.kr>
2025-12-04 13:55:27 +0900

    feat: Ag-Grid 기본 테이블 구성

--- grid/src/App.vue
+++ grid/src/App.vue
@@ -1,26 +1,53 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div>
+    <h1>AVM_BILLING</h1>
+    <div>
+        <h3>[AVM] ADOBE 사용량 측정</h3>
+        <ag-grid-vue
+            style="height: 500px; width: 100%;"
+            :rowData="rowData"
+            :columnDefs="colDefs"></ag-grid-vue>
+    </div>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from "vue";
+import { AllCommunityModule, ModuleRegistry } from "ag-grid-community";
+import { AgGridVue } from "ag-grid-vue3";
+
+ModuleRegistry.registerModules([AllCommunityModule]);
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    AgGridVue,
+  },
+  setup() {
+    // Row Data: The data to be displayed.
+    const rowData = ref([
+      { make: "Tesla", model: "Model Y", price: 64950, electric: true },
+      { make: "Ford", model: "F-Series", price: 33850, electric: true },
+      { make: "Toyota", model: "Corolla", price: 29600, electric: true },
+      { make: "Toyota", model: "Corolla", price: 29600, electric: true },
+    ]);
+
+    // Column Definitions: Defines the columns to be displayed.
+    const colDefs = ref([
+      { field: "make" },
+      { field: "model" },
+      { field: "price" },
+      { field: "electric" },
+    ]);
+
+    return {
+      rowData,
+      colDefs,
+    };
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
